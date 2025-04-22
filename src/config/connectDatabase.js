85359244fa75e80960271448f022aee797487e6e
Nam <namhuynh.101104@gmail.com>
2025-04-22 17:56:08 +0700

    Xong #8

---
+++ src/config/connectDatabase.js
@@ -0,0 +1,20 @@
+const { Sequelize } = require('sequelize');
+
+// Option 3: Passing parameters separately (other dialects)
+const sequelize = new Sequelize('room_management', 'root', '123456', {
+    host: 'localhost',
+    dialect: 'mysql',
+    logging: false, // Disable logging; default: console.log
+
+});
+
+const connectDatabase = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+export default connectDatabase;
