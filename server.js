e3a54ad472e3084ce98601e405c51259570f1ac7
Nam <namhuynh.101104@gmail.com>
2025-04-22 15:47:31 +0700

    Xong #7

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+require('dotenv').config();
+import cors from 'cors';
+import initRoutes from './src/routes';
+const app = express()
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+initRoutes(app)
+
+const port = process.env.PORT || 8888
+const listener = app.listen(port, () => {
+    console.log(`server is running on port ${listener.address().port}`)
+
+})
+
+
